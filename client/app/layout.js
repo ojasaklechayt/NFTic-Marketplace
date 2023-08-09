de386bbc4043ee23686394fd3f49c8e36584bac9
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-08-09 17:49:07 +0530

    Add files via upload

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,4 +1,20 @@
+'use client'
 import './globals.css';
+import { Web3Modal } from '@web3modal/react';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet, polygon } from 'wagmi/chains'
+
+const chains = [arbitrum, mainnet, polygon]
+const projectId = '0ac2dfebf04b585872a69ef495846553'
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
 export const metadata = {
   title: 'NFTic Marketplace',
@@ -8,7 +24,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      </body>
     </html>
   );
 }
--- client/app/navbar.js
+++ client/app/navbar.js
@@ -1,14 +1,37 @@
 'use client'
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useWeb3Modal } from '@web3modal/react'
+import { Web3Button } from '@web3modal/react'
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [walletName, setWalletName] = useState('Connect Wallet'); // Initialize walletName state
+    const { open, account } = useWeb3Modal(); // Destructure account from useWeb3Modal
+
+    useEffect(() => {
+        if (account) {
+            // Update the walletName to the first 5 characters of the wallet address
+            setWalletName(account.slice(0, 5));
+        } else {
+            setWalletName('Connect Wallet'); // Reset walletName when wallet is disconnected
+        }
+    }, [account]);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
+    const handleConnectWallet = async () => {
+        try {
+            await open();
+            console.log('Wallet connected, address:', account);
+        } catch (error) {
+            console.error('Error connecting wallet:', error);
+        }
+    };
+
+    
     return (
         <div>
             <div className="px-5 md:bg-[#D9D9D9] md:bg-opacity-20 h-[50px] flex flex-row items-center relative">
@@ -18,8 +41,8 @@ const Navbar = () => {
                     <Link href="/Collection"><p className="hover:underline opacity-80">Your Collection</p></Link>
                     <Link href="/Create"><p className="hover:underline opacity-80">Create</p></Link>
                 </div>
-                <div className="hidden md:block ml-auto">
-                    <button className="bg-white text-white text-sm font-bold p-[8px] rounded-lg bg-opacity-40 hover:bg-black">Connect Wallet</button>
+                <div className="hidden ml-auto md:block">
+                    <Web3Button icon='hide' />
                 </div>
                 <div className="ml-auto md:hidden">
                     <button
@@ -37,7 +60,8 @@ const Navbar = () => {
                     <Link href="/Explore"><p className="text-black hover:underline opacity-80 mb-2">Explore</p></Link>
                     <Link href="/Collection"><p className="text-black hover:underline opacity-80 mb-2">Your Collection</p></Link>
                     <Link href="/Create"><p className="text-black hover:underline opacity-80 mb-2">Create</p></Link>
-                    <button className="text-black bg-white text-sm font-bold p-[8px] rounded-lg bg-opacity-40">Connect Wallet</button>
+                    {/* <button onClick={handleConnectWallet} className="text-black bg-white text-sm font-bold p-[8px] rounded-lg bg-opacity-40">{walletName}</button> */}
+                    <Web3Button icon='hide'/> 
                 </div>
             )}
         </div>
