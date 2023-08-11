c21beea902935af05feff5d294873b222f37a24e
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-08-11 19:53:46 +0530

    Add files via upload

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv/config");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_URL = process.env.SEP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.19",
+  plugins: ["@nomicfoundation/hardhat-toolbox"],
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,27 +1,19 @@
-const hre = require("hardhat");
+const { ethers } = require('hardhat');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  console.log('Deploying contracts with the account:', deployer.address);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const MyContract = await ethers.getContractFactory('NFTMarket');
+  const myContract = await MyContract.deploy();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log('MyContract address:', myContract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
