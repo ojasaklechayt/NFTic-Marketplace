f6996c0456b56d504dfdf4bcb63e645fd855de85
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-08-13 23:16:26 +0530

    Add files via upload

--- client/app/page.js
+++ client/app/page.js
@@ -1,14 +1,17 @@
+'use clien'
 import Image from "next/image";
 import Navbar from "./navbar";
 import Cat from "../public/cat.png";
+import { useRouter } from 'next/navigation'
 export default function Home() {
+  let router = useRouter();
   return (
     <div className="homepage">
       <div className="absolute">
         <div className="relative">
         </div>
       </div>
-      <Navbar />
+      <Navbar router = {router} />
       <div className="flex flex-col justify-start items-center poppins h-[746px]">
         <h1 className="text-[1.6rem] md:text-[3rem] text-center mt-20">NFTic<br/> Marketplace</h1>
         <h3 className="text-[1.4rem]">Express your NFT love</h3>
